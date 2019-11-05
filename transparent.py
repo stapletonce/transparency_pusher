8694c708fcf9bfb45a439dae10223bf795988500
Chloe Stapleton <chloelstapleton@gmail.com>
2019-11-05 16:07:03 -0500

    first commit

---
+++ transparent.py
@@ -0,0 +1,13 @@
+from scapy.all import *
+
+# https://stackoverflow.com/questions/10033285/how-to-change-a-packet-data-with-scapy
+
+def chgSend(x):
+    if x[IP].dst == 'honeypot destination'      # send honeypot traffic to camera
+        x[IP].dst = 'x.x.x.x'                   # make sure packet has original internet source IP
+    else if x[IP].dst != 'honeypot IP'          # packets go back to internet
+        x[IP].src = 'honeypot IP'               # make sure source IP is the same as dest IP from first packet
+    send(x)
+
+while 1:
+    sniff(x)
